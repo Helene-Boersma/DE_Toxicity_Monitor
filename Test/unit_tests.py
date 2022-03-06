3cc2b0a3a9f8a00bc519d7346aa232e54930aea5
Guillaume Jaouen <guillaume.jaouen@efrei.net>
2022-03-06 08:05:18 -0800

    Adding a Unit Test

--- Test/unit_tests.py
+++ Test/unit_tests.py
@@ -2,8 +2,14 @@ import sys
 import pytest
 import requests
 from flask import Flask, request, render_template
-
+from model.model_toxicity import detoxify
 sys.path.append('../app_flask')
 
 def test_url_up():
     assert requests.get('http://localhost:5000').status_code == 200, "The website is broken :C"
+    
+def test_model():
+    result_model = detoxify('I love you')
+    if result_model == "This sentence is not toxic" :
+        assert False
+    else : assert True
