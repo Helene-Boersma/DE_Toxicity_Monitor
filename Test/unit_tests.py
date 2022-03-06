db5c1eece429d99e156e1a5aff91d2f4495116f6
Guillaume Jaouen <guillaume.jaouen@efrei.net>
2022-03-06 09:01:06 -0800

    Test path modifications

--- Test/unit_tests.py
+++ Test/unit_tests.py
@@ -1,15 +1,20 @@
+import os
 import sys
 import pytest
 import requests
 from flask import Flask, request, render_template
-from model.model_toxicity import detoxify
-sys.path.append('../app_flask')
+
+sys.path.insert(1, '../model')
+
+from model_toxicity import detoxify
+
 
 def test_url_up():
     assert requests.get('http://localhost:5000').status_code == 200, "The website is broken :C"
     
 def test_model():
-    result_model = detoxify('I love you')
+    x = "I hate you"
+    result_model = detoxify(x)
     if result_model == "This sentence is not toxic" :
         assert False
     else : assert True
